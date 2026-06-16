2cf469f5cc4e5e4b926bcb42655c1dde3d782ea9
Majid1383 <sammajid2540@gmail.com>
2026-06-16 16:11:03 +0530

    Initial Commit

---
+++ ImageCachingApp/ImageCachingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCachingAppApp.swift
+//  ImageCachingApp
+//
+//  Created by Abdul Majid Shaikh on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageCachingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
